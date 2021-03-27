c54bde01bbfa82b6221cc2446749d13fd035f02c
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-03-27 10:27:34 -0700

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Youssef Najjarine';
+var isCool = false;
+var totalPets = 2;
+var unknown = null;
+var never =
+console.log('fullName: ',fullName);
+console.log('isCool: ', isCool);
+console.log('totalPets: ',totalPets);
+
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ',typeof isCool);
+console.log('typeof totalPets: ',typeof totalPets);
+
+console.log('unknown: ', unknown);
+console.log('typeof unknown: ',typeof unknown);
+
+console.log('never: ',never);
+console.log('typeof never: ',never);
