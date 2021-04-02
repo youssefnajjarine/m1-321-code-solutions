1b77c0c5bb64183ea65e6869cef179ad8fce6af2
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-04-02 12:32:00 -0700

    Made changes you requested.

--- javascript-functions/main.js
+++ javascript-functions/main.js
@@ -1,8 +1,42 @@
-function addTwoNumbers(x, y) {
-  var sum = x + y;
-  return sum;
+function convertMinutesToSeconds(minutes) {
+  var convertingMinutesToSeconds = minutes * 60;
+  return convertingMinutesToSeconds;
 }
+var convertMinutesToSecondsResult = convertMinutesToSeconds(5);
+console.log('convertMinutesToSecondsResult: ', convertMinutesToSecondsResult);
 
-var addTwoNumbersResult = addTwoNumbers(2, 3);
 
-console.log('addTwoNumbersResult: ', addTwoNumbersResult);
+
+function greet(name) {
+  var greeting = 'Hey, ' + name;
+  return greeting;
+}
+var greetingResult = greet('Youssef Najjarine');
+console.log('greetingResult: ',greetingResult);
+
+
+
+function getArea(width, height) {
+  var area = width * height;
+  return area;
+}
+var getAreaResult = getArea(25, 35);
+console.log('getAreaResult: ',getAreaResult);
+
+
+
+function getFirstName(person) {
+  var getTheFirstName = person.firstName;
+  return getTheFirstName;
+}
+var getFirstNameResult = getFirstName({ firstName: 'Youssef', lastName: 'Najjarine'});
+console.log('getFirstNameResult: ',getFirstNameResult);
+
+
+
+function getLastElement(array) {
+  var lastElement = array[array.length - 1];
+  return lastElement;
+}
+var getLastElementResult = getLastElement(['propane', 'and', 'propane', 'accessories']);
+console.log('getLastElementResult: ',getLastElementResult);
