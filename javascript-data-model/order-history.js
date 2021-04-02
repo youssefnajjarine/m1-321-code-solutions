c988e88438cc1069258c497a19817d9e560a724f
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-04-02 14:58:27 -0700

    Made the changes you requested.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,63 +1,27 @@
 var orderHistory = [
   {
-    OrderPlaced: 'August 4, 2020',
-    items: [
-      {
-      OrderPlaced: 'August 4, 2020',
-      Total: '$34.00',
-      ShipTo: 'JS Masher',
-      OrderNumber: '114-3941689-8772232',
-      Delivered: 'Aug 8, 2020',
-      HandedDirectlyToResident: 'Yes',
-      Title: 'JavaScript for impatient programmers',
-      Author: 'Rauschmayer, Dr. Axel',
-      ReturnWindowClosedOn: 'Sep 7, 2020',
-      Cost: '$31.55'
-  },
-
-  {
-    OrderPlaced: 'July 19, 2020',
-    Total: '$44.53',
-    ShipTo: 'JS Masher',
-    OrderNumber: '113-9984268-1280257',
-    Delivered: 'Jul 20, 2020',
-    HandedDirectlyToResident: 'Yes',
-    Title: 'The Timeless Way of Building'
-  },
-
-  {
-    OrderNumber: '113-9984268-1280257',
-    Delivered: 'Jul 20, 2020',
-    HandedDirectlyToResident: 'Yes',
-    Title: 'The Timeless Way of Building',
-    Author: 'Alexander, Christopher',
-    ReturnWindowClosedOn: 'Aug 19, 2020',
-    Cost: '$41.33'
-  },
-
-  {
-    OrderPlaced: 'July 4, 2020',
-    Total: '$17.22',
-    ShipTo: 'JS Masher',
-    OrderNumber: '114-2875557-9059409',
-    Delivered: 'Jul 7, 2020',
-    HandedDirectlyToResident: 'Yes',
-    Title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adaptorfor Wii U, No Driver and No Lag-Gamecube Adapter',
-    ReturnWindowClosedOn: 'Aug 5, 2020',
-    Cost: '$15.98'
-  },
-
-  {
-    OrderPlaced: 'July 3, 2020',
-    Total: '$138.93',
-    ShipTo: 'JS Masher',
-    OrderNumber: '113-2883177-2648248',
-    Delivered: 'Jul 5, 2020',
-    Title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql',
-    Author: 'Faroult, Stephane',
-    ReturnWindowClosedOn: 'Aug 4, 2020',
-  }
-
-]
+    orderedDate: '2020-08-04',
+    totalPrice: 34,
+    shipTo: 'JS Masher',
+    orderNum: '114-3941689-8772232',
+    deliveredDate: '2020-08-08',
+    orderedDate: '2-15-21',
+    items: [{
+      type: "book",
+      title: "A New Way to Learn"
+    },
+    {
+      type: "electronics",
+      title: "Blu Ray Player"
+    },
+    {
+      type: "book",
+      title: "How to forget"
+    },
+    {
+      type: "book",
+      title: "how to remember"
+    }
+    ]
   }
 ]
