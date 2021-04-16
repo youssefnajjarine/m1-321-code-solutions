7dfb5a1fb62ed7f7973b8a160cb1c9aaf2e84a52
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-04-15 19:15:13 -0700

    Completed dom-querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,15 @@
+// console.log('hello, world');
+var $heading = document.querySelector('h1');
+var $explanation = document.querySelector('#explanation');
+var $hint = document.querySelector('.hint');
+var $paragraphs = document.querySelectorAll('p');
+var $links = document.querySelectorAll('.example-link');
+
+console.log('$heading', $heading);
+console.dir($heading);
+console.log('$explanation', $explanation);
+console.dir($explanation);
+console.log('$hint', $hint);
+console.dir($hint);
+console.log('$paragraphs', $paragraphs);
+console.log('$links', $links);
